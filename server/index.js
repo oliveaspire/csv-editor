315fd154c66f53cb2a31bd3258b5a86c838c408f
Yash Tripathi <126502657+oliveaspire@users.noreply.github.com>
2025-04-13 22:42:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173", // for local dev
+      "https://csv-editor-frontend.onrender.com/", 
+    ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
