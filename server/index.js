f708f00cfefa2ded34018aed3f798ee8cc31ce88
Yash Tripathi <126502657+oliveaspire@users.noreply.github.com>
2025-04-13 20:32:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { DataModel } from "./models/data.model.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
